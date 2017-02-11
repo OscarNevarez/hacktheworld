332df162607c54f438695ab2103022b0a5fad38b
Victor Darkes <josephdarkes@gmail.com>
2017-02-11 15:47:15 -0800

    express

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
+
 var express = require('express');
+//var Dropzone = require("dropzone");
+
 var app = express();
 
+//app.use(express.static('../frontend'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendfile('../frontend/index.html');
 });
 
 app.listen(3000, function () {
