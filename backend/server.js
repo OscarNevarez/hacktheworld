a90b0065d3a31de32f5641ee59d14806be054bf7
Philc90 <philipchiang5@gmail.com>
2017-02-11 19:54:49 -0800

    added connection between front and backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
-
 var express = require('express');
-//var Dropzone = require("dropzone");
 var path = require('path');
+var fileUpload = require('express-fileupload');
 
 var app = express();
-
+app.use(fileUpload());
 app.use("/",  express.static(path.resolve("../frontend/")));
 
 app.get('/', function (req, res) {
   res.sendFile(path.resolve("../frontend/static/index.html"));
+});
+
+app.post('/upload', function(req, res) {
+    console.log('/upload called');
+    if (!req.files) {
+        res.send('No files were uploaded.');
+        return;
+    }
+    console.log(req.files);
 });
 
 app.listen(3000, function () {
